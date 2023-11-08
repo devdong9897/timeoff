9def5b2883fbf8dfcf3106e43e5ddbf8f642b524
gyeongisung <gisung.gyeon@gmail.com>
2023-11-08 21:52:27 +0900

    feat: footer CSS 작업

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,16 +3,25 @@ import { FooterWrap } from "../styles/FooterStyle";
 
 const Footer = () => {
   return (
-    <>
-      <FooterWrap>
-        <ul>
-          <li>기성</li>
-          <li>민용</li>
-          <li>다혜</li>
-          <li>동욱</li>
-        </ul>
-      </FooterWrap>
-    </>
+    <FooterWrap>
+      <ul className="footer-top">
+        <li>개인정보처리방침</li>
+        <li>이용약관</li>
+        <li>저작권정책</li>
+        <li>고객센터</li>
+        <li>찾아오시는 길</li>
+      </ul>
+      <ul className="footer-bt">
+        <li>TEL: 053-123-4567</li>
+        <li>상호: 타임오프</li>
+        <li>대표: 김수한무</li>
+        <li>우)123456</li>
+        <li>대구시 중구 중앙대로 394 제일빌딩 5F</li>
+        <li>개인정보관리 책임자: 남동욱</li>
+        <li>사업자등록번호: 222-11-12345</li>
+        <li>통신판매업신고 : 제0000-대구중구-1234호</li>
+      </ul>
+    </FooterWrap>
   );
 };
 
--- src/styles/FooterStyle.ts
+++ src/styles/FooterStyle.ts
@@ -1,24 +1,33 @@
 import styled from "@emotion/styled";
 
 export const FooterWrap = styled.footer`
-  max-width: 70%;
+  max-width: 100%;
   height: 100%;
-  background: #006dab;
+  border-top: 1px solid #e6e6e6;
+  background: #f0f0f0;
   margin: 0 auto;
-  ul {
+  .footer-top {
     display: flex;
-    justify-content: space-around;
+    justify-content: center;
     align-items: center;
+    gap: 80px;
+    width: 100%;
+    height: 70px;
+    > li {
+      color: #666;
+      cursor: pointer;
+    }
+  }
+  .footer-bt {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
     width: 100%;
     height: 100%;
-    li {
-      text-align: center;
-      width: 20px;
-      height: 45px;
-      line-height: 45px;
-      font-size: 20px;
-      background: 0;
-      color: #fff;
+    margin-bottom: 20px;
+    > li {
+      color: #666;
       cursor: pointer;
     }
   }
