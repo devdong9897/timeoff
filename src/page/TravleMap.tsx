61269d70bf339525d05981c4552ab467c0d0a8f8
gyeongisung <gisung.gyeon@gmail.com>
2023-11-14 23:38:53 +0900

    feat: Naver Map 적용중

--- src/page/TravleMap.tsx
+++ src/page/TravleMap.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import NaverMap from "../components/NaverMap";
 
 const TravleMap = () => {
-  return <div>TravleMap</div>;
+  return (
+    <div>
+      <NaverMap />
+    </div>
+  );
 };
 
 export default TravleMap;
