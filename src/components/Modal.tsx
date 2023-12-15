3d37dbd5bd4a727fb7162963e8b6af80016d575e
gyeongisung <gisung.gyeon@gmail.com>
2023-12-15 15:45:58 +0900

    feat: 로그인 모달 퍼블리싱 작업

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -16,31 +16,43 @@ export const LoginModal: React.FC<PropsType> = ({
   };
 
   return (
-    <ModalWrap>
+    <>
       {modalOpen && (
-        <ModalInner>
-          <ul className="modal-header">
-            <li>
-              <h2>로그인</h2>
-            </li>
-            <li>
-              <span onClick={handleCancel}>✖</span>
-            </li>
-          </ul>
-          <div className="modal-body">
-            <span>WelCome to TIME OFF</span>
-            <span>오신걸 환영합니다.</span>
-            <ul>
-              <li></li>
-              <li></li>
+        <ModalWrap>
+          <div className="dim"></div>
+          <ModalInner>
+            <ul className="modal-top">
+              <li>
+                <h2>로그인</h2>
+              </li>
+              <li>
+                <span onClick={handleCancel}>✖</span>
+              </li>
             </ul>
-            <span>sns소셜 로그인을 통해</span>
-            <span>다양한 정보를 볼 수 있습니다.</span>
-          </div>
-        </ModalInner>
+            <div className="modal-btm">
+              <div className="modal-btm-header">
+                <span>WelCome to</span>&nbsp;
+                <span>TIME OFF</span>
+                <br />
+                <span>오신걸 환영합니다.</span>
+              </div>
+              <ul>
+                <li>
+                  <img src="/login/naver.png" alt="" />
+                </li>
+                <li>
+                  <img src="/login/kakao.png" alt="" />
+                </li>
+              </ul>
+              <div className="modal-btm-footer">
+                <span>sns소셜 로그인을 통해</span>
+                <br />
+                <span>다양한 여행 정보를 볼 수 있습니다.</span>
+              </div>
+            </div>
+          </ModalInner>
+        </ModalWrap>
       )}
-    </ModalWrap>
+    </>
   );
 };
-
-LoginModal.propTypes = {};
--- src/styles/ModalStyle.ts
+++ src/styles/ModalStyle.ts
@@ -1,38 +1,84 @@
 import styled from "@emotion/styled";
 
-export const ModalWrap = styled.div``;
+export const ModalWrap = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  .dim {
+    position: absolute;
+    width: 100%;
+    height: 100vh;
+    background: rgba(0, 0, 0, 0.3);
+    backdrop-filter: blur(2px);
+    z-index: 10;
+  }
+`;
 export const ModalInner = styled.div`
   position: absolute;
   width: 50%;
-  height: 30rem;
+  height: 38rem;
   top: 28rem;
   left: 50%;
   transform: translate(-50%, -50%);
   text-align: center;
-  background: skyblue;
+  background: #fff;
   border-radius: 10px;
-  display: flex;
-  flex-flow: column nowrap;
-  justify-content: space-between;
-  padding: 30px 0 30px 0;
   z-index: 99;
-  /* .dim {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    background: rgba(0, 0, 0, 0.3);
-    backdrop-filter: blur(2px);
-  } */
-  .modal-header {
+  .modal-top {
     display: flex;
     justify-content: center;
-    border-bottom: 1px solid black;
-    padding-bottom: 30px;
+    padding: 30px 0 30px 0;
+    border-radius: 10px 10px 0 0;
+    background: #006dab;
+    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.1);
+    li {
+      h2 {
+        font-size: 3rem;
+        font-weight: 400;
+        color: #fff;
+      }
+    }
     li:last-of-type {
       position: absolute;
       top: 2.5%;
       right: 2.5%;
       cursor: pointer;
+    }
+  }
+  .modal-btm {
+    .modal-btm-header {
+      font-size: 1.6rem;
+      padding: 30px 0 30px 0;
+      span:nth-child(2) {
+        font-size: 1.8rem;
+        font-weight: 600;
+        color: #006dab;
+      }
+    }
+    ul {
+      li {
+        cursor: pointer;
+      }
+    }
+    .modal-btm-footer {
+      font-size: 1rem;
+      padding-top: 30px;
+    }
+    ul {
+      display: flex;
+      justify-content: center;
+      gap: 3rem;
+      li {
+        width: 380px;
+        height: 230px;
+        gap: 30px;
+        img {
+          width: 380px;
+          height: 230px;
+        }
+      }
     }
   }
 `;
