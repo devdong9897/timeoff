aef9c24814c8b56c31c3f503af7d4e6746896016
hyemdev <devdev.hyem@gmail.com>
2023-12-20 10:03:19 +0900

    update : 마이페이지, 메뉴부분 수정

--- src/components/mypage/Mymenu.tsx
+++ src/components/mypage/Mymenu.tsx
@@ -2,10 +2,12 @@ import React, { useState } from "react";
 import BookmarkModal from "./BookmarkModal";
 import { MymenuSection } from "../../styles/MypageStyle";
 
+
+
 const Mymenu = () => {
   // 모달창 노출 여부 state
-  const [modalOpen, setModalOpen] = useState(false);
-  const [whatOpen, setWhatOpen] = useState("mark");
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [whatOpen, setWhatOpen] = useState<string>("mark");
 
   // 모달창 노출
   const showMarkModal = () => {
@@ -28,7 +30,9 @@ const Mymenu = () => {
           <h4>즐겨찾기</h4>
           <div className="bookmark-menu">
             <button onClick={showMarkModal}>더보기</button>
-            {modalOpen && <BookmarkModal setModalOpen={setModalOpen} whatOpen={whatOpen}/>}
+            {modalOpen && (
+              <BookmarkModal setModalOpen={setModalOpen} whatOpen={whatOpen} />
+            )}
             {/* {modalOpen && <FavoriteBookmark setModalOpen={setModalOpen} />} */}
           </div>
         </li>
@@ -36,7 +40,9 @@ const Mymenu = () => {
           <h4>코스</h4>
           <div className="course-menu">
             <button onClick={showCourseModal}>더보기</button>
-            {modalOpen && <BookmarkModal setModalOpen={setModalOpen} whatOpen={whatOpen} />}
+            {modalOpen && (
+              <BookmarkModal setModalOpen={setModalOpen} whatOpen={whatOpen} />
+            )}
             {/* {modalOpen && <FavoriteCourse setModalOpen={setModalOpen} />} */}
           </div>
         </li>
